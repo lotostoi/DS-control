7f035c198f281f0f114448a5a5a42d596cff7287
lotostoii <lotostoii@gmail.com>
2020-06-29 17:40:09 +1000

    переписывем код

--- frontend/js/zoom.js
+++ frontend/js/zoom.js
@@ -5,27 +5,18 @@ window.onload = () => {
 
     let initialized = [];
 
-  
-    function Magnify(el, opts, i) {
-        if (!~initialized.indexOf(el)) {
-            initialized.push(el);
 
+    class Magnify {
+        constructor(el, opts, i) {
             this.opts = opts;
-            this.image = anchor = el;
-
-            this.alter = el.getAttribute('data-magnify-src')
-                || (anchor && anchor.getAttribute('href'))
-                || opts.src
-                || '';
+            this.image = el;
+            this.alter = el.getAttribute('data-magnify-src') || opts.src || '';
 
             this.wrap(i);
 
             this.zoom(i);
         }
-    }
-
-    Magnify.prototype = {
-        wrap: function (i) {
+        wrap(i) {
             let inst = this,
                 image = inst.image,
                 container, lens;
@@ -48,9 +39,8 @@ window.onload = () => {
             inst.container = container;
             inst.lens = lens;
 
-        },
-
-        zoom: function (i) {
+        }
+        zoom(i) {
             let inst = this,
                 container = inst.container,
                 lens = null,
@@ -141,8 +131,8 @@ window.onload = () => {
             container.addEventListener('mousemove', function (e) {
                 let box = d.querySelector(`div[data-id = "${i}"]`).getBoundingClientRect(),
                     clientX = e.clientX
-                    clientY = e.clientY
-          
+                clientY = e.clientY
+
                 socket.emit('mousemove_soket', {
                     box: box,
                     clientX: clientX,
@@ -163,7 +153,7 @@ window.onload = () => {
                         clientX = data.data.clientX * kw,
                         clientY = data.data.clientY * kh;
 
-        
+
                     // Will always true with pointer-events: none
                     if (clientX > box.left && box.right > clientX && clientY > box.top && box.bottom > clientY) {
                         calc(clientX, clientY, box.left, box.top)
@@ -185,11 +175,11 @@ window.onload = () => {
                 if (e.pointerType == "touch" && !e.isPrimary) {
                     return
                 }
-              
+
                 let box = d.querySelector(`div[data-id = "${i}"]`).getBoundingClientRect(),
                     clientX = e.touches[0].clientX,
                     clientY = e.touches[0].clientY;
-         
+
                 socket.emit('touchmove_soket', {
                     box: box,
                     clientX: clientX,
@@ -209,8 +199,8 @@ window.onload = () => {
                     let box = d.querySelector(`div[data-id = "${data.data.i}"]`).getBoundingClientRect(),
                         clientX = data.data.clientX * d.body.clientWidth / data.data.vueportW,
                         clientY = data.data.clientY * d.body.clientHeight / data.data.vueportH;
-                  
-           
+
+
                     // Will always true with pointer-events: none
                     if (clientX > box.left && box.right > clientX && clientY > box.top && box.bottom > clientY) {
                         calc(clientX, clientY, box.left, box.top)
@@ -266,7 +256,10 @@ window.onload = () => {
                 lens.className = 'magnify-lens magnify-error';
             });
         }
-    };
+
+    }
+
+
 
     window.magnify = function (el, opts) {
         let i;
