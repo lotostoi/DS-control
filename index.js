9de884385eaec2f42d6b79e51c5a82153d92cc27
lotostoii <lotostoii@gmail.com>
2020-06-29 17:07:38 +1000

    add server

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ let server = require('http').createServer(express)
 
 let io = require('socket.io').listen(server)
 
-server.listen(port)
-console.log(`Приложение запущенно на - http://localhost:${port}/`)
+server.listen(port, () => {
+    console.log(`Главный экран - http://localhost:${port}/`)
+    console.log(`Управялющий экран - http://localhost:${port}/control`)
+})
+
 
 
 express.get('*', (request, respons) => {
@@ -14,6 +17,9 @@ express.get('*', (request, respons) => {
     if (request.path.search(/.jpg/) == -1) {
         if (request.path == '/') {
             respons.sendFile(__dirname + '/frontend/index.html')
+
+        } else if (request.path == '/control') {
+            respons.sendFile(__dirname + '/frontend/control.html')
         } else {
             respons.set('Content-Type', 'text/css');
             respons.sendFile(__dirname + request.path)
@@ -25,27 +31,33 @@ express.get('*', (request, respons) => {
     }
 })
 
+let connections = [],
+    numcon = 0;
 
 
 io.sockets.on('connection', function (socket) {
-    console.log(socket.id)
+
+    numcon++
+    connections.push(socket.id)
+   
+    io.sockets.emit('sendid', { numCon: numcon, connections: connections, id: socket.id })
+
+
 
     socket.on('clickTest', (data) => {
-     
         io.sockets.emit('kk', { id: data })
     })
 
-   
+
     socket.on('mous active', (data) => {
-       
-        io.sockets.emit('sendMous', { id:socket.id , cor: data })
+
+        io.sockets.emit('sendMous', { id: socket.id, cor: data })
     })
 
     // события лупы
 
     socket.on('mouseover_soket', (data) => {
-      
-        io.sockets.emit('mouseover_soket_server', { id: socket.id, data: data  })
+        io.sockets.emit('mouseover_soket_server', { id: socket.id, data: data })
     })
 
     socket.on('mouseleave_soket', (data) => {
@@ -53,7 +65,7 @@ io.sockets.on('connection', function (socket) {
     })
 
     socket.on('mousemove_soket', (data) => {
-      
+
         io.sockets.emit('mousemove_soket_server', { id: socket.id, data: data })
     })
     socket.on('load_soket', (data) => {
@@ -72,13 +84,11 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('touchend_soket_server', { id: socket.id, data: data })
     })
 
-    socket.on('touchmove_soket', (data) => {    
+    socket.on('touchmove_soket', (data) => {
 
         io.sockets.emit('touchmove_soket_server', { id: socket.id, data: data })
 
     })
-
-
 
 });
 
