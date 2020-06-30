66be665c1509ae2e21489d3bda58ffc027bcdb98
lotostoii <lotostoii@gmail.com>
2020-06-30 14:47:32 +1000

    плохой варант

--- frontend/js/zoom.js
+++ frontend/js/zoom.js
@@ -7,30 +7,36 @@ class Slider {
         this.back = document.querySelector(`${back}`)
         this.next = document.querySelector(`${next}`)
         this.currentVal = 0
-        this.befotVal = this.list.length + 1
+        this.befotVal = 0
+        this.cl_right = 'right'
+        this.cl_left = 'left'
         this._handler()
         this._addDataId()
+        // this.list[this.currentVal].style.opacity = 1 
     }
     _handler() {
-        document.querySelector('body').addEventListener('click', e => {
+        document.documentElement.addEventListener('click', e => {
             e.preventDefault()
+            this.befotVal=this.currentVal
             if (e.target.parentNode.className == this.next.className) {
                 if (++this.currentVal > this.list.length - 1) {
-                    this.currentVal = 0
-                }
-                this._next(this.currentVal)
+                    this.currentVal = 0      
+                } 
+
+                this._slide(this.cl_right)
             }
             if (e.target.parentNode.className == this.back.className) {
                 if (--this.currentVal < 0) {
                     this.currentVal = this.list.length - 1
-                }
-                this._back(this.currentVal)
+                } 
+                this._slide(this.cl_left)
+
             }
         })
 
         let start = null, finish = null
 
-        document.querySelector('body').addEventListener('touchstart', (e) => {
+        document.documentElement.addEventListener('touchstart', (e) => {
             //e.preventDefault()
             if (e.pointerType == "touch" && !e.isPrimary) {
                 return
@@ -39,34 +45,39 @@ class Slider {
         })
 
 
-        document.querySelector('body').addEventListener('touchmove', (e) => {
+        document.documentElement.addEventListener('touchmove', (e) => {
             // e.preventDefault()
             if (e.pointerType == "touch" && !e.isPrimary) {
                 return
             }
 
             finish = e
-            
+
 
         })
 
 
-        document.querySelector('body').addEventListener("touchend", (e) => {
-            e.preventDefault()
+        document.documentElement.addEventListener("touchend", (e) => {
+           // e.preventDefault()
             if (e.pointerType == "touch" && !e.isPrimary) {
                 return
             }
-            console.log(start + " " + finish)
+         //   console.log(start + " " + finish)
             if (start && finish) {
+                this.befotVal=this.currentVal
                 if (start.touches[0].pageX < finish.touches[0].pageX) {
+                   
                     if (++this.currentVal > this.list.length - 1) {
-                        this.currentVal = 0
-                    } this._next(this.currentVal)
+                        this.currentVal = 0      
+                    } 
+    
+                    this._slide(this.cl_right)
 
                 } else {
                     if (--this.currentVal < 0) {
                         this.currentVal = this.list.length - 1
-                    } this._back(this.currentVal)
+                    } 
+                    this._slide(this.cl_left)
 
                 }
             }
@@ -79,24 +90,19 @@ class Slider {
             e.setAttribute('data-id', i)
         })
     }
-    _setZindex() {
-        this.list.forEach((e, i) => {
-            e.style.zIndex = '10'
-            e.classList.remove('right')
-            e.classList.remove('left')
-        })
-    } _next(id) {
-        let el = document.querySelector(`div[data-id="${id}"]`)
-        this._setZindex()
-        el.classList.add('right')
-        el.style.zIndex = '30'
+    _slide(cl) {
+  
+      
+        d.querySelector(`div[data-id="${this.currentVal}"]`).classList.add(cl)
+        setTimeout(()=>{
+            d.querySelector(`div[data-id="${this.befotVal}"]`).classList.remove('right')
+            d.querySelector(`div[data-id="${this.befotVal}"]`).classList.remove('left')
+         
+        }, 1100) 
+       
+
     }
-    _back(id) {
-        let el = document.querySelector(`div[data-id="${id}"]`)
-        this._setZindex()
-        el.classList.add('left')
-        el.style.zIndex = '30'
-    }
+
 }
 
 
