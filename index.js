d1650eba5204c4e5bf8d3fda252cbd5d4b660b92
lotostoi <53521245+lotostoi@users.noreply.github.com>
2020-07-15 00:08:23 +1000

    new Pictures

--- index.js
+++ index.js
@@ -41,64 +41,10 @@ express.post('/getImg', (request, respons) => {
     respons.send(scanFolder("./frontend/img", "./frontend/img/"))
 })
 
-let connections = [],
-    numcon = 0;
+
 
 
 io.sockets.on('connection', function (socket) {
-
-    numcon++
-    connections.push(socket.id)
-
-    io.sockets.emit('sendid', { numCon: numcon, connections: connections, id: socket.id })
-
-
-
-    socket.on('clickTest', (data) => {
-        io.sockets.emit('kk', { id: data })
-    })
-
-
-    socket.on('mous active', (data) => {
-
-        io.sockets.emit('sendMous', { id: socket.id, cor: data })
-    })
-
-    // события лупы
-
-    socket.on('mouseover_soket', (data) => {
-        io.sockets.emit('mouseover_soket_server', { id: socket.id, data: data })
-    })
-
-    socket.on('mouseleave_soket', (data) => {
-        io.sockets.emit('mouseleave_soket_server', { id: socket.id, data: data })
-    })
-
-    socket.on('mousemove_soket', (data) => {
-
-        io.sockets.emit('mousemove_soket_server', { id: socket.id, data: data })
-    })
-    socket.on('load_soket', (data) => {
-        io.sockets.emit('load_soket_server', { id: socket.id, data: data })
-    })
-
-
-    // события лупы сенсорный экран 
-
-    socket.on('touchstart_soket', (data) => {
-
-        io.sockets.emit('touchstart_soket_server', { id: socket.id, data: data })
-    })
-
-    socket.on('touchend_soket', (data) => {
-        io.sockets.emit('touchend_soket_server', { id: socket.id, data: data })
-    })
-
-    socket.on('touchmove_soket', (data) => {
-
-        io.sockets.emit('touchmove_soket_server', { id: socket.id, data: data })
-
-    })
 
 
     socket.on('touchLeft', () => {
